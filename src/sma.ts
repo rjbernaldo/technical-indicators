e02c51a26ec939635ca4ab50641a0b1b5a9a2691
Rj Bernaldo <rj.bernaldo@gmail.com>
2019-09-02 17:54:41 +1000

    Fix prettier

--- src/sma.ts
+++ src/sma.ts
@@ -1,6 +1,8 @@
 // https://www.investopedia.com/terms/s/sma.asp
 export default function(dataSet: number[], period: number): number {
-  if (dataSet.length < period) return NaN;
+  if (dataSet.length < period) {
+    return NaN;
+  }
   const total = dataSet.slice(-period).reduce((acc, cur) => acc + cur, 0);
   return total / period;
 }
