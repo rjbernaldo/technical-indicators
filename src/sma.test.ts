022d9abbb0f49d1f1c7e3763ec919dc67283c652
Rj Bernaldo <rj.bernaldo@gmail.com>
2019-09-02 17:53:57 +1000

    Add various test cases based on the platform's usecase

--- src/sma.test.ts
+++ src/sma.test.ts
@@ -1,7 +1,42 @@
 import sma from './sma';
 
-test('sma', () => {
-  const result = sma([1, 2, 3], 3);
+test('regular', () => {
+  const dataSet = [1, 2, 3];
+  const result = sma(dataSet, 3);
 
   expect(result).toBe(2);
 });
+
+test('period greater than dataSet length', () => {
+  const dataSet = [1, 2, 3];
+  const result = sma(dataSet, 4);
+
+  expect(result).toBe(NaN);
+});
+
+test('overflowing data set', () => {
+  const dataSet = [ 10410,
+    10398,
+    10399,
+    10384.4799991,
+    10394.50847478,
+    10401,
+    10364,
+    10388.07693079,
+    10437,
+    10474,
+    10528,
+    10519,
+    10471,
+    10510.05063801,
+    10487,
+    10559,
+    10532.65998619,
+    10584,
+    10622.86524859,
+    10439 ]
+
+  const result = sma(dataSet, 10);
+
+  expect(result).toBe(10525.257587279);
+})
--- src/sma.ts
+++ src/sma.ts
@@ -1,5 +1,6 @@
 // https://www.investopedia.com/terms/s/sma.asp
 export default function(dataSet: number[], period: number): number {
-  const total = dataSet.reduce((acc, cur) => acc + cur, 0);
+  if (dataSet.length < period) return NaN;
+  const total = dataSet.slice(-period).reduce((acc, cur) => acc + cur, 0);
   return total / period;
 }
