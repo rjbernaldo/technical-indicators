a764945f6ab22b5bc29f6cb2ce45de9b8dfb60d7
Rj Bernaldo <rj.bernaldo@gmail.com>
2019-09-02 17:34:46 +1000

    Remove lib folder

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import sma from './sma';
-declare const _default: {
-    sma: typeof sma;
-};
-export default _default;
--- lib/sma.d.ts
+++
@@ -1,1 +0,0 @@
-export default function (dataSet: number[], period: number): number;
